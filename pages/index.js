e8b22261a7b35f7f220c31a967e1a137d6c38948
oelnajmi <oelnajmi@gmail.com>
2022-09-29 18:01:01 -0400

    added footer component

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 import Discover from '../components/Discover';
 import FeedHeader from '../components/FeedHeader';
+import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import Trending from '../components/Trending';
 
 const Home = () => {
   return (
-    <main className="min-h-screen flex flex-col max-w-[1500px] mx-auto">
+    <main className=" min-h-screen flex flex-col max-w-[1500px] mx-auto">
       <Navbar />
       <FeedHeader />
       <Discover />
       <Trending />
+      <Footer />
     </main>
   );
 };
